ca5d9087bb02f367cfa5c261237d1dffc5409bb1
Katie Belanger <belangerkat@gmail.com>
2019-12-23 17:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for ([i = 0, i < 4, i++]) {
+    console.log(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
